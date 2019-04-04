cf64eb7d8ff74d66d57d5c4d6f9e692258ded4c5
Austin Sanders <sanderau@oregonstate.edu>
2019-04-04 15:14:35 -0700

    successfully import data into numpy and assignment compliant formats

--- src/linearRegression/main.py
+++ src/linearRegression/main.py
@@ -1,2 +1,43 @@
+import numpy as np
 
-print("Hello world!")
+path = "../../docs/housing_train.txt"
+
+def countLines():
+	with open(path) as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1;
+
+def getData(X,Y, numLines):
+	Y = np.zeros(shape=(numLines, 1))
+	X = np.zeros(shape=(numLines, 13))
+	
+	with open(path) as data:
+		line = data.readline()
+		cnt = 0
+
+		while line:
+			split = line.split()
+
+			for i in range(0,12):
+				X[cnt][i] = float(split[i])
+				
+			Y[cnt][0] = float(split[13])
+
+			cnt = cnt + 1
+			line = data.readline()
+	return X, Y
+
+
+def main():
+	numLines = countLines()
+
+	Y = np.zeros(shape=(numLines, 1))
+	X = np.zeros(shape=(numLines, 13))
+
+	X, Y = getData(X,Y, numLines)
+
+	print(X)
+
+if __name__ == "__main__":
+	main()
